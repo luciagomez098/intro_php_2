345150b067bd4f7ecd92a91057f80f46c71d2d30
luciagomez098 <adrianafuentes@colegioguanenta.edu.co>
2025-05-07 17:40:41 -0500

    se incluyo index.php, modelo/, pagina_usuario.php, registrar_empleado.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion a php</title>
+</head>
+<body>
+    <h1> Pagina principal del sitio <h1>
+    <h2> adriana lucia fuentes gomez <h2>
+
+    <form action = "modelo/loguear.php"  method = "post">
+        <h3> Iniciar sesion <h3>
+        <label for="">E-mail:</label>
+        <input type= "text" name= "email" id="" require>
+        <br><br> 
+        <label for="">Password:</label>
+        <input type= "text" name= "password" id="" require>
+        <br><br> 
+        <button type ="submit"> Ingresar </button>
+    </form>
+
+    <h2> Email: adrianafuentes@colegioguanenta.edu.co <h2><br>
+    <h2> Contra: 12345<h2><br>
+</body>
+</html>
